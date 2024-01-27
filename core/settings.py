aa44aaa17d943c03e92683361711431536ec4ab6
9rosLove <yarik2300@gmail.com>
2024-01-27 15:12:06 +0200

    add crispy_bootstrap4, crispy_forms, user apps to INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_bootstrap4",
+    "crispy_forms",
+    "user",
 ]
 
 MIDDLEWARE = [
