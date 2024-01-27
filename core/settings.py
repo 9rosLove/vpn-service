4df9c974c3fe5e0cd8667a52c37950e48a96cd1b
9rosLove <yarik2300@gmail.com>
2024-01-27 15:45:22 +0200

    update AUTH_USER_MODEL and LOGIN/LOGOUT_REDIRECT_URL values in settings.py

--- core/settings.py
+++ core/settings.py
@@ -102,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
+
+LOGIN_REDIRECT_URL = "vpn:site-list"
+LOGOUT_REDIRECT_URL = "login"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
